f5c49ec884b5f6034a07909528ae12eb29174265
oloursel-ops <oloursel@hotmail.com>
2026-07-16 21:27:08 +0200

    Add public site configuration

---
+++ assets/js/site-config.js
@@ -0,0 +1,7 @@
+/* Public site configuration. Never place platform passwords or API secrets here. */
+window.VILLA_CONFIG = {
+  inquiryEmail: "oloursel@hotmail.com",
+  abritelUrl: "https://www.abritel.fr/location-vacances/p8160054a",
+  ownerRezWidgetEnabled: false,
+  ownerRezPropertyId: "ORP490143"
+};
